c757f7533ac4731dbda5acefb81d204599ebf56c
OliaK <olgamr@walla.com>
2024-08-18 20:43:12 +0300

    Improved Cards style

--- frontend/src/pages/UrlForm.jsx
+++ frontend/src/pages/UrlForm.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-import "../assets/css/UrlForm.css";
 import MetadataView from "./MetadataView";
+import "../assets/css/UrlForm.css";
 
 const UrlForm = () => {
   const [urls, setUrls] = useState(["", "", ""]);
   const [metadata, setMetadata] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  // const navigate = useNavigate();
 
   const handleUrlChange = (index, value) => {
     const updatedUrls = [...urls];
@@ -43,7 +41,6 @@ const UrlForm = () => {
     const fetchedMetadata = await fetchMetadataForUrls();
     if (fetchedMetadata) {
       setMetadata(fetchedMetadata);
-      // navigate("/metadata");
     }
 
     setLoading(false);
@@ -80,23 +77,6 @@ const UrlForm = () => {
       {error && <p style={{ color: "red" }}>{error}</p>}
 
       <MetadataView metadata={metadata} />
-
-      {/* <div>
-        {metadata.map((data, index) => (
-          <div key={index}>
-            <h4>Metadata for {data.url}</h4>
-            <p>Title: {data.title || "N/A"}</p>
-            <p>Description: {data.description || "N/A"}</p>
-            {data.image && (
-              <img
-                src={data.image}
-                alt="Preview"
-                style={{ width: "100px" }}
-              />
-            )}
-          </div>
-        ))}
-      </div> */}
     </div>
   );
 };
