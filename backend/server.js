e8d9931fcaf67009c6578392aeedf0b3ed977a9a
OliaK <olgamr@walla.com>
2024-08-18 21:05:20 +0300

    Applied rate limiting to the server

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const cors = require("cors");
 const metadataRoutes = require("./routes/metadataRoutes");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", metadataRoutes);
+const limiter = rateLimit({
+  windowMs: 1000,
+  max: 5,
+  message:
+    "You have reached the maximum requests limit, please try again later.",
+});
+
+app.use("/api", limiter, metadataRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
