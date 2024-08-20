1f2d5eaf89ff2305512348aa54b81c6db7d10121
OliaK <olgamr@walla.com>
2024-08-20 04:32:45 +0300

    Adding .env.production file

--- frontend/src/pages/UrlForm.jsx
+++ frontend/src/pages/UrlForm.jsx
@@ -16,13 +16,16 @@ const UrlForm = () => {
 
   const fetchMetadataForUrls = async () => {
     try {
-      const response = await fetch("http://localhost:5000/api/fetch-metadata", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ urls }),
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/fetch-metadata`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ urls }),
+        }
+      );
       if (!response.ok) {
         throw new Error("Failed to fetch metadata");
       }
